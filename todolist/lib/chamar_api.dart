b52b500ea51de90228ef599512ab10118a1f5351
mvfernando <you@example.com>
2023-03-17 01:01:58 +0100

    update function callApi, Add Inloader, add function post

--- todolist/lib/chamar_api.dart
+++ todolist/lib/chamar_api.dart
@@ -18,18 +18,22 @@ class chamar_api extends StatefulWidget {
 
 class _chamar_api extends State<chamar_api> {
 
-  ValueNotifier <int> valorAleatorio = ValueNotifier <int> (0);
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
+  ValueNotifier<bool> inLoader = ValueNotifier<bool>(false);
 
   CallAPI() async {
     var client = http.Client();
         try {
+          inLoader.value = true;
           var response = await client.get(
               Uri.parse('https://jsonplaceholder.typicode.com/posts'),
           );
-          var decodedResponse = jsonDecode(response.body);
-          print(decodedResponse);
+          var decodedResponse = jsonDecode(response.body) as List;
+          posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+          await Future.delayed(Duration(seconds: 2));
         } finally {
           client.close();
+          inLoader.value = false;
     }
   }
 
@@ -41,11 +45,15 @@ class _chamar_api extends State<chamar_api> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ValueListenableBuilder(
-              valueListenable: valorAleatorio,
-              builder: (_, value, __) => Text(
-                'Valor é: $value',
-                style: TextStyle(fontSize: 25),
+            AnimatedBuilder(
+              animation: Listenable.merge([posts, inLoader]),
+              builder: (_, __) => ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: posts.value.length,
+                itemBuilder: (_, idx ) => ListTile(
+                  title: Text(posts.value[idx].title),
+                ),
               ),
             ),
             SizedBox(height: 18),
@@ -59,5 +67,23 @@ class _chamar_api extends State<chamar_api> {
         ),
       ),
     );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body );
+
+  factory Post.fromJson(Map json){
+    return Post(json['userId'], json['id'], json['title'], json['body']);
+  }
+
+  @override
+  String toString(){
+    return 'id: $id';
   }
 }
